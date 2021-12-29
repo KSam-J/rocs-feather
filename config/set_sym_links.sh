853243f03e70e3b5a8baf4e843238cd14f5c4e71
Kellen J. Sam <kellen.sam@bench.com>
2021-12-28 23:24:54 -0700

    FIX config symlink make relative

--- config/set_sym_links.sh
+++ config/set_sym_links.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # bash files
-ln -sf bash_files/bash_aliases ~/.bash_aliases
-ln -sf bash_files/bash_profile ~/.bash_profile
-ln -sf bash_files/bashrc ~/.bashrc
+ln -sfr bash_files/bash_aliases ~/.bash_aliases
+ln -sfr bash_files/bash_profile ~/.bash_profile
+ln -sfr bash_files/bashrc ~/.bashrc
 
 # vim
-ln -sf vim/.vimrc ~/.vimrc
+ln -sfr vim/.vimrc ~/.vimrc
 
 # alacritty
 if [[ ! -d ~/.config/alacritty ]]; then
     mkdir ~/.config/alacritty
 fi
-ln -sf alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sfr alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # tmux
-ln -sf tmux/.tmux.conf ~/.tmux.conf
+ln -sfr tmux/.tmux.conf ~/.tmux.conf
 
 # git
-ln -sf git_files/gitconfig ~/.gitconfig
+ln -sfr git_files/gitconfig ~/.gitconfig
 
