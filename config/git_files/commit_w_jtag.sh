120be95b4ff475253de3812cd186e9b5dd3ba5ef
Kellen J. Sam <kellen.sam@bench.com>
2022-09-20 17:31:44 -0700

    Fix ONE_LINER logic in commit_w_jtag

--- config/git_files/commit_w_jtag.sh
+++ config/git_files/commit_w_jtag.sh
@@ -5,19 +5,20 @@ ONE_LINER="${1}"
 J_TAG_REGEX='^[A-Z]{1,4}-\d{1,4}'
 
 # Get the Jira Tag from the branch name
-# function
 J_TAG=$(git branch --show-current | grep --perl-regexp --only-matching "${J_TAG_REGEX}")
 
 # Check that J_TAG is populated
-if [[ -z ${J_TAG} ]]; then
-    J_TAG='NOTRACKER'
-fi
-# potential function end
-
-# Take J_TAG and attempt to attach ONE_LINER
-if [[ -n ${ONE_LINER} ]]; then
-    git commit --message="[${J_TAG}] ${ONE_LINER}"
+if [[ -n ${J_TAG} ]]; then
+    if [[ -n ${ONE_LINER} ]]; then
+        git commit --message="[${J_TAG}] ${ONE_LINER}"
+    else
+        git commit --message="[${J_TAG}] " --edit
+    fi
 else
-    git commit --message="[${J_TAG}] " --edit
+    if [[ -n ${ONE_LINER} ]]; then
+        git commit --message="[NOTRACKER] ${ONE_LINER}" --edit
+    else
+        git commit --message="[NOTRACKER] " --edit
+    fi
 fi
 
