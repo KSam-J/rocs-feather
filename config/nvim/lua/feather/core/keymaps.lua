9b6e8cff3cbca210cf289a63b063435fae68f122
Kellen J. Sam <samkel@bench.com>
2024-08-27 13:15:53 -0700

    Comment out <leader>f, fuzzy-find

--- config/nvim/lua/feather/core/keymaps.lua
+++ config/nvim/lua/feather/core/keymaps.lua
@@ -19,3 +19,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tc", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- local wk = require("which-key")
+-- wk.add({
+--   { "<leader>f", group = "fuzzy-find" }, -- group
+-- })
