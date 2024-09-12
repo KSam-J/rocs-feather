2a9a93375d9c33cc0293bba8bba01c9e58d31bc4
Kellen J. Sam <samkel@bench.com>
2024-09-12 11:33:38 -0700

    [NOTRACKER] Move update-alternative-terminal.sh

--- scripts/update-alternative-terminal.sh
+++ scripts/update-alternative-terminal.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Set alacritty as the default terminal using update-alternatives
+#
+# Does not care about CWD
+#
+# Assumes alacritty has already been installed
 
-# Check script was run with sudo
-if [ "$EUID" -ne 0 ];then
-    echo "Please run as root (i.e. sudo)"
-    exit 1
-fi
 
 ALACRITTY_LOCATION="$(which alacritty)"
 
@@ -16,8 +15,8 @@ fi
 
 # Add alacritty to the alternatives list
 # --install <link> <name> <path> <priority>
-update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 1
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator ${ALACRITTY_LOCATION} 1
 
 # Set alacritty to be the default terminal
 # --set <name> <path>
-sudo update-alternatives --set x-terminal-emulator /usr/local/bin/alacritty
+sudo update-alternatives --set x-terminal-emulator ${ALACRITTY_LOCATION}
