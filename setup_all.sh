cd2a4ee67a8012ddc6bf8c866a240c6b73b79937
Kellen J. Sam <samkel@bench.com>
2024-09-12 11:44:38 -0700

    [NOTRACKER] Make setup_all.sh useful

--- setup_all.sh
+++ setup_all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Copy over the bashrc and aliases
-cd config/bash_files
-. ./setup.sh
+# Set all symlinks
+cd config # scripts expect to be run in their dir
+. ./set_sym_links.sh
 cd ..
+
+# Install apt packages
+. linux_tools.sh
+
+# Make bash tab completes case-insensitive
+. config/bash_files/case-insensitive.sh
