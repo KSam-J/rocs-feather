1977009314cd0f381780f9383c9f5728717ecaa6
Kellen J. Sam <samkel@bench.com>
2025-01-13 15:00:37 -0700

    symlink force in set_symlinks.sh

--- config/set_sym_links.sh
+++ config/set_sym_links.sh
@@ -1,28 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+# Create a .config if necessary
+if [[ ! -d ~/.config ]]; then
+    mkdir ~/.config
+fi
 
 # bash files
-ln -sir bash_files/bash_aliases ~/.bash_aliases
-ln -sir bash_files/bash_profile ~/.bash_profile
-ln -sir bash_files/bashrc ~/.bashrc
+ln -sif bash_files/bash_aliases ~/.bash_aliases
+ln -sif bash_files/bash_profile ~/.bash_profile
+ln -sif bash_files/bashrc ~/.bashrc
 
 # vim
-ln -sir vim/.vimrc ~/.vimrc
+ln -sif vim/.vimrc ~/.vimrc
 
 # alacritty
 if [[ -d ~/.config/alacritty ]]; then
-    rmdir ~/.config/alacritty
+    mv ~/.config/alacritty ~/.config/alacritty.old
 fi
-ln -si alacritty/ ~/.config/alacritty
+ln -sif alacritty/ ~/.config/alacritty
 
 # tmux
-ln -sir tmux/.tmux.conf ~/.tmux.conf
+ln -sif tmux/.tmux.conf ~/.tmux.conf
 
 # git
-ln -sir git_files/gitconfig ~/.gitconfig
-sudo ln -sir git_files/commit_w_jtag.sh /usr/bin/
+ln -sif git_files/gitconfig ~/.gitconfig
+sudo ln -sif git_files/commit_w_jtag.sh /usr/bin/
 
 # nvim
-ln -sir nvim ~/.config/nvim
+ln -sif nvim ~/.config/nvim
 
 # starship
-ln --symbolic --interactive --relative starship/starship.toml ~/.config/
+ln --symbolic --interactive --relative --force starship/starship.toml ~/.config/
+
+# fish
+if [[ -d ~/.config/fish ]]; then
+    mv ~/.config/fish ~/.config/fish.old
+fi
+
+ln --symbolic --interactive --force --relative fish ~/.config/
+
