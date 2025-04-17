0a99a0ae0e39131804d9b69519740ac574aaf45e
Kellen J. Sam <samkel@bench.com>
2025-04-17 10:27:25 -0700

    [NOTRACKER] save and execute rust_installer in tmp/

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rust_installer.sh
+
+sudo chmod +x /tmp/rust_installer
+/tmp/rust_installer.sh --no-modify-path
 
