259451c495d11d05f11d7cbbb1d2d5f4cf0174b7
Kellen J. Sam <samkel@bench.com>
2025-09-05 10:43:29 -0700

    Disable Lazy Mason workaround

--- config/nvim/lua/feather/plugins/mason-workaround.lua
+++ config/nvim/lua/feather/plugins/mason-workaround.lua
@@ -1,4 +1,5 @@
+-- Disabled this workaround on 9/5/25, after some time of stability, DELETE ME
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
