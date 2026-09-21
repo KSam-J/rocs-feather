3c31f9b9a2f2741260855991dc95fdd5b09882e5
Kellen J. Sam <kellonasam@gmail.com>
2026-09-21 15:10:23 -0700

    Simplify fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,2 @@
-cd (dirname (status --current-filename))
 
-# Define the input and output file paths
-set input_file "../bash_files/bash_aliases"
-set output_file "/tmp/fish_aliases"
-
-
-# Translate bash aliases
-./bash_to.fish  $input_file $output_file
-
-source $output_file
+starship init fish | source
