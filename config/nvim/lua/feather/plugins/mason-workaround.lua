c8d0e52f6a7d7e195454136eaed219824c5543d2
Kellen J. Sam <kellonasam@gmail.com>
2026-09-25 11:55:46 -0700

    nvim: Remove old mason-workaround plugin

--- config/nvim/lua/feather/plugins/mason-workaround.lua
+++
@@ -1,5 +0,0 @@
--- Disabled this workaround on 9/5/25, after some time of stability, DELETE ME
-return {
-  -- { "mason-org/mason.nvim", version = "^1.0.0" },
-  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
